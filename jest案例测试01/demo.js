0a9535b838bbabbb29d3d8f0dced22054b917a46
ji4396 <1187939346@qq.com>
2021-08-03 08:51:14 +0800

    新的js-demo分支

---
+++ jest案例测试01/demo.js
@@ -0,0 +1,7 @@
+function reduce(a,b){
+    return a -b 
+}
+
+module.exports = {
+    reduce
+}
---
+++ jest案例测试01/demo.test.js
@@ -0,0 +1,5 @@
+const {reduce} =require('./demo')
+
+test('测试reduce',()=>{
+    expect(reduce(4,2)).toBe(2)
+})
